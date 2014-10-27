ec8705c384a467dfb28cc38ed4a6813a6a30a1c4
tdmcarthur <travis.d.mcarthur@gmail.com>
2014-10-27 12:46:04 -0500

    Started work on occupation data file

--- econ-718-paper.r
+++ econ-718-paper.r
@@ -1,2 +1,35 @@
-# test
-# second test
+
+
+if (Sys.info()["user"]=="travismcarthur") {
+  work.dir <- "/Users/travismcarthur/Desktop/Dropbox/718 paper/data/"
+}
+
+if (Sys.info()["user"]=="patchachaikitmongkol") {
+  work.dir <- ""
+  # MINT: Put the directory of your data folder above
+}
+
+
+
+library("foreign")
+
+oww3.df <- read.dta(paste0(work.dir, "oww3.dta"))
+
+oww3.occ.classif.df <- read.csv("/Users/travismcarthur/Desktop/Dropbox/718 paper/data/oww3 occ classification.csv", stringsAsFactors=FALSE)
+
+oww3.ind.classif.df <- read.csv("/Users/travismcarthur/Desktop/Dropbox/718 paper/data/oww3 industry classification.csv", stringsAsFactors=FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
